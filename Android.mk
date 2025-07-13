0de7ebf04a47537ef715ffe866b2868c2716a9d7
Mathias Gluszczynski <admin@krazey.de>
2025-07-13 14:57:35 +0200

    star2lte: Move audio configuration to the device tree

--- Android.mk
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright (C) 2022 The LineageOS Project
-
-LOCAL_PATH := $(call my-dir)
-
-ifneq ($(filter star2lte, $(TARGET_DEVICE)),)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
