af909dfe0d603ab6c536f2a2eb69fce3f7ce9898
Mathias Gluszczynski <admin@krazey.de>
2025-10-19 12:47:20 +0200

    star2lte: Move vendor securitypatch to common
    Signed-off-by: Mathias Gluszczynski <admin@krazey.de>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,9 +9,6 @@ TARGET_SCREEN_DENSITY := 560
 # Kernel
 TARGET_KERNEL_CONFIG := exynos9810-star2lte_defconfig
 
-# Vendor Security Patch
-VENDOR_SECURITY_PATCH := 2022-03-01
-
 # Inherit from the common tree
 include device/samsung/exynos9810-common/BoardConfigCommon.mk
 
